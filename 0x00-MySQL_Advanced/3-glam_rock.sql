c8c0e82c3a2121403f6d3cb592c1650f4bf7469b
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-07-11 12:56:50 +0100

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,4 @@
--- Lists all bands with Glam rock as their style,
--- ranked by their longevity
--- Column names must be: band_name & lifespan
-
-SELECT band_name, ifnull(split, 2020)-ifnull(formed, 0) AS lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC
+-- lists all bands with Glam rock
+SELECT band_name, COALESCE(split, 2022) - formed AS lifespan
+FROM metal_bands WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
