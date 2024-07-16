5ff61cba88c158494ec5c497b0d494d1908cc155
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-07-16 15:07:12 +0100

    Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,45 +1,26 @@
 #!/usr/bin/env python3
-""" Log stats - new version """
+"""Defines a function that  provides some stats
+   about Nginx logs stored in MongoDB
+"""
 from pymongo import MongoClient
 
 
-def nginx_stats_check():
-    """ provides some stats about Nginx logs stored in MongoDB:"""
+def log_stats():
+    """Provides some stats about Nginx logs stored in MongoDB"""
     client = MongoClient()
     collection = client.logs.nginx
 
-    num_of_docs = collection.count_documents({})
-    print("{} logs".format(num_of_docs))
+    doc_count = collection.count_documents({})
+    print(f"{doc_count} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
-    methods_list = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods_list:
+    for method in methods:
         method_count = collection.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, method_count))
-    status = collection.count_documents({"method": "GET", "path": "/status"})
-    print("{} status check".format(status))
-
-    print("IPs:")
-
-    top_IPs = collection.aggregate([
-        {"$group":
-         {
-             "_id": "$ip",
-             "count": {"$sum": 1}
-         }
-         },
-        {"$sort": {"count": -1}},
-        {"$limit": 10},
-        {"$project": {
-            "_id": 0,
-            "ip": "$_id",
-            "count": 1
-        }}
-    ])
-    for top_ip in top_IPs:
-        count = top_ip.get("count")
-        ip_address = top_ip.get("ip")
-        print("\t{}: {}".format(ip_address, count))
-
+        print(f"\tmethod {method}: {method_count}")
+    status_check = collection.count_documents({"method": "GET",
+                                                "path": "/status"})
+    print(f"{status_check} status check")
 
 if __name__ == "__main__":
-    nginx_stats_check()
+    log_stats()
